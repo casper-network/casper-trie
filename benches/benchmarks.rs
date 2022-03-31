eb0a3beb624c75a0f4184001feade8da41987625
Matthew Doty <mpwd@casperlabs.io>
2022-03-30 22:39:25 -0400

    Fixing linear congruential generator

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -7,7 +7,7 @@ use trie::{
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     // LCG from https://www.ams.org/journals/mcom/1999-68-225/S0025-5718-99-00996-5/S0025-5718-99-00996-5.pdf
-    let a: u128 = 82461096547334812307256211668490605096;
+    let a: u128 = 13891176665706064842;
     let m: u128 = (1 << 64) - 59;
 
     c.bench_function("Store 10_000 leaves", |b| {
@@ -16,7 +16,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             let mut updater = Updater::new(&mut store, EMPTY_TRIE_ROOT);
             let mut x: u128 = a;
             for _ in 0..10_000 {
-                x = x.saturating_add(a) % m;
+                x = (x * a) % m;
                 updater
                     .put((x as u64).to_le_bytes().as_ref(), &[])
                     .expect("could not put");
